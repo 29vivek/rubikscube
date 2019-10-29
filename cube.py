08cd59601636470cf0f464309303b552cc7c8b9c
Vivek Kekuda <vivekkekuda29@gmail.com>
2019-10-30 03:31:49 +0530

    multiprocessing

--- cube.py
+++ cube.py
@@ -1,17 +1,33 @@
 import cv2
 import numpy as np
-from multiprocessing import Pool, Array
+from multiprocessing import Pool
 import time
 import os
+import multiprocessing
+import multiprocessing.pool
+class NoDaemonProcess(multiprocessing.Process):
+    @property
+    def daemon(self):
+        return False
 
-# import multiprocessing
-# We must import this explicitly, it is not imported by the top-level multiprocessing module.
-# import multiprocessing.pool
+    @daemon.setter
+    def daemon(self, value):
+        pass
 
-arr = Array('i', 54, lock=False)
+class NoDaemonContext(type(multiprocessing.get_context())):
+    Process = NoDaemonProcess
 
 # We sub-class multiprocessing.pool.Pool instead of multiprocessing.Pool
 # because the latter is only a wrapper function, not a proper class.
+class MyPool(multiprocessing.pool.Pool):
+    def __init__(self, *args, **kwargs):
+        kwargs['context'] = NoDaemonContext()
+        super(MyPool, self).__init__(*args, **kwargs)
+
+
+arr = multiprocessing.Array('i', 54, lock=False)
+file_names=['left.png','front.png','right.png','back.png','up.png','down.png']
+dim = (300,300)
 
 lower_white = ((0,0,100),)
 upper_white = ((180,150,255),)
@@ -33,30 +49,30 @@ upper_yellow = ((50, 255, 255),)
 
 def show(im, x):
     cv2.imshow('im{}'.format(x), im)
-    cv2.waitKey(0) # milliseconds
+    cv2.waitKey(500) # milliseconds
     cv2.destroyAllWindows()
 
 def facelet_number(x_cen,y_cen):
-    if x_cen>=0 and x_cen<=100:
+    if x_cen>=0 and x_cen<=dim[0]/3:
         row=0
-    elif x_cen>100 and x_cen<=200:
+    elif x_cen>dim[0]/3 and x_cen<=2*dim[0]/3:
         row=1
-    elif x_cen>200 and x_cen<=300:   
+    elif x_cen>2*dim[0]/3 and x_cen<=dim[0]:   
         row=2
-    if y_cen>=0 and y_cen<=100:
+    if y_cen>=0 and y_cen<=dim[1]/3:
         column=0
-    elif y_cen>100 and y_cen<=200:
+    elif y_cen>dim[1]/3 and y_cen<=2*dim[1]/3:
         column=3
-    elif y_cen>200 and y_cen<=300:   
+    elif y_cen>2*dim[1]/3 and y_cen<=dim[1]:   
         column=6
     return row+column   
 
 def get_color_number(thresh):
-    if thresh==lower_orange:
+    if thresh==lower_red:
         return 0
     if thresh==lower_blue:
         return 1
-    if thresh==lower_red:
+    if thresh==lower_orange:
         return 2
     if thresh==lower_green:
         return 3
@@ -66,9 +82,7 @@ def get_color_number(thresh):
         return 5
 
 def find(obj):
-    dim = (300,300)
-
-    obj[0] = cv2.resize(obj[0], dim)
+    
     obj[0] = cv2.bilateralFilter(obj[0], 9, 75, 75)
     # print(obj[0].shape)
     
@@ -109,7 +123,8 @@ def find(obj):
         num=facelet_number(x_cen,y_cen)
         arr[obj[1]*9+num]=get_color_number(obj[2])
     
-    # show(obj[0],7)
+    # show(obj[0], 8)
+    return obj[0]
     
 def run_in_parallel(im, face):
     ranges = [[im, face, lower_white, upper_white], 
@@ -119,7 +134,29 @@ def run_in_parallel(im, face):
               [im, face, lower_red, upper_red],
               [im, face, lower_yellow, upper_yellow]]
     pool = Pool(processes = len(ranges))
-    pool.map(find, ranges)
+    results = pool.map(find, ranges)
+    pool.close()
+    pool.join()
+
+    color_order = ['white', 'blue', 'orange', 'green', 'red', 'yellow']
+    for i, result in enumerate(results):
+        cv2.imwrite(file_names[face].split('.')[0]+'_{}.png'.format(color_order[i]), result)
+
+def run_just_linear(im, face):
+    ranges = [[im, face, lower_white, upper_white], 
+              [im, face, lower_blue, upper_blue],
+              [im, face, lower_orange, upper_orange],
+              [im, face, lower_green, upper_green],
+              [im, face, lower_red, upper_red],
+              [im, face, lower_yellow, upper_yellow]]
+    
+    results = []
+    for color in ranges:
+        results.append(find(color))
+
+    color_order = ['white', 'blue', 'orange', 'green', 'red', 'yellow']
+    for i, result in enumerate(results):
+        cv2.imwrite(file_names[face].split('.')[0]+'_{}.png'.format(color_order[i]), result)
 
 def get_char(temp):
     if temp==0:
@@ -135,21 +172,65 @@ def get_char(temp):
     if temp==5:
         return 'D'
 
+def run_face_parallel(obj):
+    # only obj is defined, which is say left.png
+    im = cv2.imread(obj)
+    im = cv2.resize(im, dim)
+    run_in_parallel(im, file_names.index(obj))
+
+def run_face_linear(obj):
+    im = cv2.imread(obj)
+    im = cv2.resize(im, dim)
+    run_just_linear(im, file_names.index(obj))
+
+
 if __name__ == '__main__':
-    t1=time.time()
     
-    file_names=['left.png','front.png','right.png','back.png','up.png','down.png']
+    t1=time.time()
+
+    pool = MyPool(processes=len(file_names))
+    pool.map(run_face_parallel, file_names)
+    pool.close()
+    pool.join()
+
+    t2=time.time()
+
+    print('execution time for parallel face, parallel color: ', t2-t1)
+
+    t1=time.time()
+
+    pool = MyPool(processes=len(file_names))
+    pool.map(run_face_linear, file_names)
+    pool.close()
+    pool.join()
+
+    t2=time.time()
+
+    print('execution time for parallel face, linear color: ', t2-t1)
+
+
+    t1=time.time()
     
     for i in range(len(file_names)):
         im=cv2.imread(file_names[i])
+        im = cv2.resize(im, dim)
         run_in_parallel(im, i)
     
     t2=time.time()
     
-    print('execution time: ', t2-t1)
-    # for x in arr:
-    #     print(x, end=' ')
+    print('execution time for linear face, parallel color: ', t2-t1)
     
+    t1=time.time()
+    
+    for i in range(len(file_names)):
+        im=cv2.imread(file_names[i])
+        im = cv2.resize(im, dim)
+        run_just_linear(im, i)
+    
+    t2=time.time()
+
+    print('execution time for linear face, linear color: ', t2-t1)
+
     edges_num = [[43,10],[41,19],[37,28],[39,1],
                 [46,16],[50,25],[52,34],[48,7],
                 [14,21],[12,5],[30,23],[32,3]]
@@ -173,5 +254,6 @@ if __name__ == '__main__':
 
     cubeString = ' '.join(edges_char + corner_char)
     print('input: ', cubeString)
+    os.popen('gcc ./solver/ct5.C -w')
     solution = os.popen('./a.out ' + cubeString).read()
     print('solution: ', solution)
